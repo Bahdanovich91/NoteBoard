01ce72668442c932b805366ff96eca3f6c18dac8
Bahdanovich <bahdanovich@internet.ru>
2022-11-19 19:01:02 +0300

    Fixed code style in selected_note.blade.php

--- resources/views/notes/selected_note.blade.php
+++ resources/views/notes/selected_note.blade.php
@@ -1,29 +1,27 @@
 @extends('layouts.app')
 
 @if($data)
+    @section('title', $data->name)
 
-@section('title', $data->name)
+    @section('content')
 
-@section('content')
+        @include('inc.header')
 
-    @include('inc.header')
+        @include('inc.messages')
 
-    @include('inc.messages')
-
-    <div id="notes" class="mt-4">
-        <h2>Selected note</h2>
-        <div class="alert alert-info">
-            <h3>{{ $data->name }}</h3>
-            <p>{{ $data->description }}</p>
-            <a href="{{ route('note_update', $data->id) }}">
-                <button>Edit</button>
-            </a>
-            <a href="{{ route('note_delete', $data->id) }}">
-                <button>Delete</button>
-            </a>
+        <div id="notes" class="mt-4">
+            <h2>Selected note</h2>
+            <div class="alert alert-info">
+                <h3>{{ $data->name }}</h3>
+                <p>{{ $data->description }}</p>
+                <a href="{{ route('note_update', $data->id) }}">
+                    <button>Edit</button>
+                </a>
+                <a href="{{ route('note_delete', $data->id) }}">
+                    <button>Delete</button>
+                </a>
+            </div>
         </div>
-    </div>
 
-@endsection
-
+    @endsection
 @endif
